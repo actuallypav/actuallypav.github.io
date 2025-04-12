272e178c453785a3fe95e23b291e2c935c79e2b3
actuallypav <pawel.ambrozy@pm.me>
2025-04-12 03:01:34 +0100

    add in a better import for commands

--- commands.js
+++ commands.js
@@ -1,24 +1,36 @@
-import help from './commands/help.js';
-import about from './commands/about.js';
-import clear from './commands/clear.js';
+// import help from './commands/help.js';
+// import about from './commands/about.js';
+// import clear from './commands/clear.js';
 
-const commandMap = {
-    help,
-    about,
-    clear,
+// const commandMap = {
+//     help,
+//     about,
+//     clear,
+// };
+
+const commandMap = {};
+const loadCommand = (commandName) => {
+    import(`./commands/${commandName}.js`)
+    .then((commandModule) => {
+        commandMap[commandName] = commandModule.default;
+    })
+    .catch((err) => {
+        console.error(`Failed to load command ${commandName}:`, err);
+    });
 };
 
-export function runCommand(cmd, username, hostname, write) {
+const commandNames = ['help', 'about', 'clear', 'ls'];
+commandNames.forEach(loadCommand)
+
+export function runCommand(cmdLine, username, hostname, write, env) {
+    const [cmd, ...args] = cmdLine.split(' ');
     const command = commandMap[cmd];
 
     if (command) {
-        if (cmd === 'clear') {
-            command(write, true);  // explicitly pass true to trigger clear
-        } else {
-            command(write);
-        }
+            command(write, args, env);  // explicitly pass true to trigger clear
     }
     else if (cmd !== '') {
         write(`${cmd}: command not found`);
     }
 }
+
