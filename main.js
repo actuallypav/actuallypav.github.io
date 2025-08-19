3cf9a4a12718bfa94b130c1d49d14f8d28c23700
pav <61046893+actuallypav@users.noreply.github.com>
2025-08-19 09:41:45 +0100

    update prompt message

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { initQuickbarTerminalBindings } from './quicklinks/quickbar.js';
 const term = document.getElementById('terminal');
 initializeTerminal(term)
 
-let username = localStorage.getItem('username') || (prompt('Enter your username: ')?.trim() || 'user');
+let username = localStorage.getItem('username') || (prompt('Enter your username (will default to "user" if left empty): ')?.trim() || 'user');
 let hostname = 'ubuntu-web-terminal';
 let cwd = `/home`;
 let buffer = '';
