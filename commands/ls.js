d5b94122445d00f0f5f726289de0657e3c9f47ee
actuallypav <pawel.ambrozy@pm.me>
2025-08-22 00:36:54 +0100

    dynamic blog ls

--- commands/ls.js
+++ commands/ls.js
@@ -1,4 +1,5 @@
 import { getNodeFromPath } from "../vfs.js";
+import { list as listBlog } from '../content/blogLoader.js';
 
 export const description = `ls: ls [path]
     List files and directories.
@@ -19,7 +20,6 @@ export const description = `ls: ls [path]
         0  if files and directories are listed.
         >0 if the directory is invalid.`;
 
-
 export default function ls(write, args, { path }) {
     let pathToCheck = path;
 
@@ -29,6 +29,21 @@ export default function ls(write, args, { path }) {
             : (path.endsWith('/') ? path + args[0] : path + '/' + args[0]);
     }
 
+    //check for dynamic blog/archive dirs first
+    const dynamic = await (async () => {
+        if (pathToCheck === '/blog') return await listBlog('/blog');
+        const m = pathToCheck.match(/^\/old_posts\/\d{4}\/\d{2}$/);
+        if (m) return await listBlog(pathToCheck);
+        return null;
+    })();
+
+    if (dynamic) {
+        const formatted = dynamic.map(p =>
+            `<a href="#" class="fs-link" data-fs-type="file" data-fs-path="${p.path}">${p.date}-${p.title.replace(/</g,'&lt;')}</a>`
+        );
+        return write(formatted.join('  '));
+    }
+
     const currentDirNode = getNodeFromPath(pathToCheck);
 
     if (currentDirNode && currentDirNode.type === 'dir') {
@@ -36,7 +51,8 @@ export default function ls(write, args, { path }) {
         if (children.length > 0) {
             const formattedChildren = children.map(child => {
                 const childNode = currentDirNode.children[child];
-                return childNode.type === 'dir' ? `<span class="directory">${child}</span>` : child;
+                return childNode.type === 'dir' ? `<span class="directory">${child}</span>` 
+                : child;
             });
             write(formattedChildren.join('  '));
         } else {
