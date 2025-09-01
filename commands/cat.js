3a5dc90003036a79342063c8cac07e2b4a5279e5
actuallypav <pawel.ambrozy@pm.me>
2025-09-01 10:55:37 +0100

    newline tests

--- commands/cat.js
+++ commands/cat.js
@@ -3,7 +3,7 @@ import { marked } from "https://cdn.jsdelivr.net/npm/marked@12.0.2/lib/marked.es
 
 marked.use({
   gfm: true,
-  breaks: true,
+  breaks: false,
 });
 
 export const description = `cat: cat [file|pattern]
